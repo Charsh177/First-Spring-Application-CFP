ba7602c20bf560d2f0661a64990d892a4875c824
Charsh177 <harshchougule177@gmail.com>
2022-08-25 01:50:57 +0530

    ADDED: Creates Controller and User Model for First Spring App Development

---
+++ src/test/java/com/firstspringapp/SpringAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppDevelopmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
